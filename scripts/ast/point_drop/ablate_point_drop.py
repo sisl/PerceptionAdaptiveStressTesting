6cc94540f074ce2b9ae1a50320fde2cd86df80c1
Harrison Delecki <hdelecki@stanford.edu>
2021-09-30 09:59:27 -0700

    Add runner scripts for ast experiments

---
+++ scripts/ast/point_drop/ablate_point_drop.py
@@ -0,0 +1,127 @@
+import pickle
+import yaml
+from runner_mcts_point_drop import runner as mcts_runner
+
+
+if __name__ == '__main__':
+    # metrics = ['MinADEK']*3 + ['MinFDEK']*3
+    # ks = [10, 5, 1, 10, 5, 1]
+    # thresholds = [4, 6, 8, 10, 15, 20]
+    metrics = ['MinFDEK']*7
+    ks = [5]*7
+    #ks = [1]*4
+    thresholds = [10, 12, 14, 16, 18, 20, 22]
+    drop_likelihood = 0.1
+
+    for i in range(len(metrics)):
+        metric = metrics[i]
+        k = ks[i]
+        threshold = thresholds[i]
+        base_log_dir = './de7_6a4/' + metric + '-' + str(k) + '-'+ str(threshold) + '-' + str(drop_likelihood)
+
+
+        # Overall settings
+        max_path_length = 10
+        s_0 = [0]
+        #base_log_dir = './ade10_run'
+        # experiment settings
+        run_experiment_args = {'snapshot_mode': 'last',
+                            'snapshot_gap': 1,
+                            'log_dir': None,
+                            'exp_name': None,
+                            'seed': 0,
+                            'n_parallel': 4,
+                            'tabular_log_file': 'progress.csv'
+                            }
+
+        # runner settings
+        runner_args = {'n_epochs': 100,
+                    'batch_size': 100,
+                    'plot': False
+                    }
+
+        # env settings
+        env_args = {'id': 'ast_toolbox:Perception',
+                    'blackbox_sim_state': True,
+                    'open_loop': False,
+                    'fixed_init_state': True,
+                    's_0': s_0,
+                    }
+
+        # simulation settings
+        pipeline_config_path = '/home/hdelecki/ford_ws/SequentialPerceptionPipeline/configs/pipeline_config.yaml'
+        with open(pipeline_config_path, 'r') as f:
+            pipeline_config = yaml.load(f, yaml.SafeLoader)
+
+        sim_args = {'blackbox_sim_state': True,
+                    'open_loop': False,
+                    'fixed_initial_state': True,
+                    'max_path_length': max_path_length,
+                    }
+
+        scene_token ='de7d80a1f5fb4c3e82ce8a4f213b450a' #'fcbccedd61424f1b85dcbf8f897f9754'
+        target_instance = '6a4b9f7abafd44f38ff670c3064b13dc' # '045cd82a77a1472499e8c15100cb5ff3'
+        max_range = 35
+        pipeline_args = {'pipeline_config': pipeline_config,
+                        'scene_token': scene_token,
+                        'target_instance': target_instance,
+                        'drop_likelihood': drop_likelihood,
+                        'eval_metric': metric,
+                        'eval_k': k,
+                        'eval_metric_th': threshold,
+                        'max_range': max_range,
+                        'no_pred_failure': True
+                        }
+
+        # reward settings
+        reward_args = {'use_heuristic': False}
+
+        # spaces settings
+        spaces_args = {'rsg_length': 4}
+
+        # MCTS Settings ----------------------------------------------------------------------
+        mcts_type = 'mcts'
+
+        mcts_sampler_args = {}
+
+        mcts_algo_args = {'max_path_length': max_path_length,
+                            'stress_test_mode': 2,
+                            'ec': 100.0,
+                            'n_itr': 50,
+                            'k': 0.5,
+                            'alpha': 0.5,
+                            'clear_nodes': True,
+                            'log_interval': 50,
+                            'plot_tree': False,
+                            'plot_path': None,
+                            'log_dir': None,
+                            }
+
+        mcts_bpq_args = {'N': 3}
+
+        # MCTS settings
+        run_experiment_args['log_dir'] = base_log_dir + '/mcts'
+        run_experiment_args['exp_name'] = 'mcts'
+
+        mcts_algo_args['max_path_length'] = max_path_length
+        mcts_algo_args['log_dir'] = run_experiment_args['log_dir']
+        mcts_algo_args['plot_path'] = run_experiment_args['log_dir']
+
+        try:
+            mcts_runner(
+                mcts_type=mcts_type,
+                env_args=env_args,
+                run_experiment_args=run_experiment_args,
+                sim_args=sim_args,
+                perception_args=pipeline_args,
+                reward_args=reward_args,
+                spaces_args=spaces_args,
+                algo_args=mcts_algo_args,
+                runner_args=runner_args,
+                bpq_args=mcts_bpq_args,
+                sampler_args=mcts_sampler_args,
+                save_expert_trajectory=True,
+            )
+        except:
+            pass
+
---
+++ scripts/ast/point_drop/runner_mcts_point_drop.py
@@ -0,0 +1,191 @@
+import fire
+import numpy as np
+import torch
+# Useful imports
+# from example_save_trials import *
+import tensorflow as tf
+# Import the necessary garage classes
+# from garage.baselines.linear_feature_baseline import LinearFeatureBaseline
+# from garage.misc import logger
+from garage.experiment import run_experiment
+from garage.tf.experiment import LocalTFRunner
+
+import ast_toolbox.mcts.BoundedPriorityQueues as BPQ
+from ast_toolbox.algos import MCTS
+from ast_toolbox.algos import MCTSBV
+from ast_toolbox.algos import MCTSRS
+# Import the AST classes
+from ast_toolbox.envs import ASTEnv
+#from ast_toolbox.rewards import ExampleAVReward
+from ast_toolbox.samplers import ASTVectorizedSampler
+#from ast_toolbox.simulators import ExampleAVSimulator
+#from ast_toolbox.spaces import ExampleAVSpaces
+
+from nuscenes.nuscenes import NuScenes
+from sequential_perception.classical_pipeline import build_pipeline
+from sequential_perception.ast import PerceptionASTSimulator, PerceptionASTSpaces, PerceptionASTReward
+
+
+def build_simulator(perception_args, sim_args):
+        # Create NuScenes
+    pipeline_config = perception_args['pipeline_config']
+    nuscenes = NuScenes(version=pipeline_config['NUSCENES_VERSION'],
+                        dataroot=pipeline_config['NUSCENES_DATAROOT'],
+                        verbose=True)
+
+    # Create Pipeline
+    pipeline = build_pipeline(pipeline_config, nuscenes)
+
+    # Create Simulator
+    sim = PerceptionASTSimulator(nuscenes, pipeline, perception_args=perception_args, **sim_args)
+
+    return sim
+
+def runner(
+    mcts_type=None,
+    env_args=None,
+    run_experiment_args=None,
+    sim_args=None,
+    perception_args=None,
+    reward_args=None,
+    spaces_args=None,
+    algo_args=None,
+    runner_args=None,
+    bpq_args=None,
+    sampler_args=None,
+    save_expert_trajectory=False,
+    
+    # log_dir='.',
+):
+    if mcts_type is None:
+        mcts_type = 'mcts'
+
+    if env_args is None:
+        env_args = {}
+
+    if run_experiment_args is None:
+        run_experiment_args = {}
+
+    # if sim_args is None:
+    #     sim_args = {}
+    if sim_args is None:
+        sim_args = {}
+
+    if perception_args is None:
+        raise NotImplementedError()
+
+    if reward_args is None:
+        reward_args = {}
+
+    if spaces_args is None:
+        spaces_args = {}
+
+    if algo_args is None:
+        algo_args = {}
+
+    if runner_args is None:
+        runner_args = {}
+
+    if sampler_args is None:
+        sampler_args = {}
+
+    if bpq_args is None:
+        bpq_args = {}
+
+    if 'n_parallel' in run_experiment_args:
+        n_parallel = run_experiment_args['n_parallel']
+    else:
+        n_parallel = 1
+        run_experiment_args['n_parallel'] = n_parallel
+
+    if 'max_path_length' in sim_args:
+        max_path_length = sim_args['max_path_length']
+    else:
+        max_path_length = 5
+        sim_args['max_path_length'] = max_path_length
+
+    if 'batch_size' in runner_args:
+        batch_size = runner_args['batch_size']
+    else:
+        batch_size = max_path_length * n_parallel
+        runner_args['batch_size'] = batch_size
+
+    def run_task(snapshot_config, *_):
+
+        seed = 0
+        # top_k = 10
+        np.random.seed(seed)
+
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config=config) as sess:
+            with tf.variable_scope('AST', reuse=tf.AUTO_REUSE):
+
+                with LocalTFRunner(
+                        snapshot_config=snapshot_config, max_cpus=4, sess=sess) as local_runner:
+
+                    # Instantiate AST classes
+                    sim = build_simulator(perception_args, sim_args)
+                    reward_function = PerceptionASTReward(**reward_args)
+                    spaces = PerceptionASTSpaces(**spaces_args)
+
+                    # Create the environment
+                    if 'id' in env_args:
+                        env_args.pop('id')
+                    env = ASTEnv(simulator=sim,
+                                 reward_function=reward_function,
+                                 spaces=spaces,
+                                 **env_args
+                                 )
+
+                    top_paths = BPQ.BoundedPriorityQueue(**bpq_args)
+
+                    if mcts_type == 'mcts':
+                        print('mcts')
+                        algo = MCTS(env=env,
+                                    top_paths=top_paths,
+                                    **algo_args)
+                    elif mcts_type == 'mctsbv':
+                        print('mctsbv')
+                        algo = MCTSBV(env=env,
+                                      top_paths=top_paths,
+                                      **algo_args)
+                    elif mcts_type == 'mctsrs':
+                        print('mctsrs')
+                        algo = MCTSRS(env=env,
+                                      top_paths=top_paths,
+                                      **algo_args)
+                    else:
+                        raise NotImplementedError
+
+                    sampler_cls = ASTVectorizedSampler
+                    sampler_args['sim'] = sim
+                    sampler_args['reward_function'] = reward_function
+
+                    local_runner.setup(algo=algo,
+                                       env=env,
+                                       sampler_cls=sampler_cls,
+                                       sampler_args=sampler_args)
+
+                    # Run the experiment
+                    local_runner.train(**runner_args)
+
+                    log_dir = run_experiment_args['log_dir']
+
+                    # if save_expert_trajectory:
+                    #     load_convert_and_save_mcts_expert_trajectory(
+                    #         best_actions_filename=log_dir + '/best_actions.p',
+                    #         expert_trajectory_filename=log_dir + '/expert_trajectory.p',
+                    #         sim=sim,
+                    #         s_0=env_args['s_0'],
+                    #         reward_function=reward_function)
+
+    run_experiment(
+        run_task,
+        **run_experiment_args,
+    )
+
+
+if __name__ == '__main__':
+    fire.Fire()
